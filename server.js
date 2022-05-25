0376bec3303dd66bdb346339416e6a944de65e05
ZahraO1 <zahra.masroor6@gmail.com>
2022-05-25 19:48:32 -0400

    Created Hello World! API

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
+//Languages:
+const languagesController = require('./controllers/languages_controller.js')
+//if the URL has /languages, the languagesControllers will be accessed for
+//further routing
+app.use('/languages', languagesController)
+
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
